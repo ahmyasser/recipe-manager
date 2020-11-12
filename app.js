620cdf10d07c6f05597cdee28343fb20efe92933
Ahmed Yasser <ahmedtricker@gmial.com>
2020-11-12 03:52:50 +0200

    user model done and auth routes done

--- app.js
+++ app.js
@@ -16,3 +16,21 @@ mongoose.connection.on('connected',()=>{
 mongoose.connection.on('error',(err)=>{
     console.log("err connecting",err)
 })
+
+require('./models/user')
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+
+
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
+app.listen(PORT,()=>{
+    console.log("server is running on",PORT)
+})
