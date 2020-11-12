a240a821e9653ec2fcb914d2c24e362c2e99ee51
Ahmed Yasser <ahmedtricker@gmial.com>
2020-11-12 03:22:33 +0200

    initialized project

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const mongoose  = require('mongoose')
+const PORT = process.env.PORT || 5000
+const {MONGOURI} = require('./config/keys')
+
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+
+})
+mongoose.connection.on('connected',()=>{
+    console.log("conneted to mongodb")
+})
+mongoose.connection.on('error',(err)=>{
+    console.log("err connecting",err)
+})
---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {  MONGOURI: "mongodb+srv://traildb19912:WzfeODgtvhz9HOE0@cluster0.pynpu.mongodb.net/note-api?retryWrites=true&w=majority", JWT_SECRET:'ddfdsfskomlkam'};
+
+
