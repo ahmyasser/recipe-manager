46bbf8734c85d72c0f55b59d018d9de99b469998
Ahmed Yasser <ahmedtricker@gmial.com>
2020-11-14 01:07:31 +0200

    signup screen done styling missing

--- client/src/Screens/Signup.js
+++ client/src/Screens/Signup.js
@@ -1,10 +1,74 @@
+import React,{useState} from 'react'
+import {Link,  useHistory} from 'react-router-dom';
 
-const Signin=() => {
-  return (
-    <h1>
-    Signup
-    </h1>
-      );
+const Signup = ()=>{
+    const history = useHistory();
+    const [name,setName] = useState("")
+    const [password,setPasword] = useState("")
+    const [email,setEmail] = useState("")
+    const uploadFields = ()=>{
+        if(!/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+        console.log('invalid email');
+        return;
+        }
+        fetch("/signup",{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                name,
+                password,
+                email
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+           if(data.error){
+              console.log({html: data.error,classes:"#c62828 red darken-3"});
+           }
+           else{
+               console.log({html:data.message,classes:"#43a047 green darken-1"});
+               history.push('/login');
+           }
+        }).catch(err=>{
+            console.log(err);
+        })
+    }
+    const PostData = ()=>uploadFields();
+
+    return( <div className="mycard">
+    <div className="card auth-card input-field">
+      <h2>Guestbook</h2>
+      <input
+      type="text"
+      placeholder="name"
+      value={name}
+      onChange={(e)=>setName(e.target.value)}
+      />
+      <input
+      type="text"
+      placeholder="email"
+      value={email}
+      onChange={(e)=>setEmail(e.target.value)}
+      />
+      <input
+      type="password"
+      placeholder="password"
+      value={password}
+      onChange={(e)=>setPasword(e.target.value)}
+      />
+      <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+      onClick={()=>PostData()}
+      >
+          SignUP
+      </button>
+      <h5>
+          <Link to="/signin">Login</Link>
+      </h5>
+
+  </div>
+</div>
+    );
 }
 
-export default Signin;
+export default Signup;
