ad8ad1bba0d4fd0754cf94894cd0ef68b05e45ec
Ahmed Yasser <ahmedtricker@gmial.com>
2020-11-14 02:51:20 +0200

    home screen done styling missing

--- client/src/Screens/Home.js
+++ client/src/Screens/Home.js
@@ -1,10 +1,64 @@
+import React,{useState,useEffect} from 'react'
+//import {Link} from 'react-router-dom'
 
-const Home=() => {
-    return (
-      <h1>
-      Home
-      </h1>
-        );
-  }
+const Home  = ()=>{
+    const [data,setData] = useState([])
+    useEffect(()=>{
+       fetch('/posts',{
+           headers:{
+               "Authorization":"Bearer "+localStorage.getItem("jwt")
+           }
+       }).then(res=>res.json())
+       .then(result=>{
+           console.log(result)
+           setData(result.post)
+       }).catch((err)=>{
+         console.log(err);
+       }
+       )
+    },[])
+
+    const deletePost = (postid)=>{
+        fetch(`/post/${postid}`,{
+            method:"delete",
+            headers:{
+                Authorization:"Bearer "+localStorage.getItem("jwt")
+            }
+        }).then(res=>res.json())
+        .then(result=>{
+            console.log(result)
+            const newData = data.filter(item=>{
+                return item._id !== result._id
+            })
+            setData(newData)
+        })
+    }
+   return (
+          data?       <div>
+           {
+               data.map(item=>{
+                   return(
+                       <div key={item._id}>
+                            <h1 >
+                              <i className="material-icons" onClick={()=>deletePost(item._id)}>delete</i>
+                            
+                            </h1>
+                            <div>
+                                <img src={item.pic} alt=""/>
+                            </div>
+                                <h1>{item.title}</h1>
+                                <p>{item.recipe}</p>
+                                <p>{item.ingredient} </p>
+                            </div>
+
+                   )
+               })
+           }
+          
+          
+       </div>:
+       <h1>err</h1>
+   )
+}
   
-  export default Home;
+export default Home;
